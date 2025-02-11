91575d64f43380612f9324eb2a50a57841a4be76
prithveeraaja <133484943+prithveeraaja@users.noreply.github.com>
2025-02-11 14:41:17 +0000

    variable and data type

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "prithvirajshinde123@gmail.com"
+var accountPassword = "1234"
+accountCity = "kol"
+let accountDetail 
+
+// if we cannot define value to variable it will remains undefined 
+
+//accountId = 2// Not allowed we can't change const value 
+
+accountEmail = "hc@hc.com"
+accountPassword = "qwert"
+accountCity = "Pune"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountDetail])
+
+/*
+prefernot to use var 
+because of issue in block scope and functional scope   {--scope--}
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict0";  //treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs, not brower 
+
+console.log(3 + 3);
+
+console.log("Prithvi")
+
+// code readibility should be high
+
+
+let name = "Prithvi"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => used for uniqueness
+
+// objects
+
+console.log(typeof"Prithvi");   // string
+console.log(typeof null);       //**********object*************
+console.log(typeof undefined);  // --undefined------
+
+
