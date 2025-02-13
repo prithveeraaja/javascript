d1cc791646e0bdb3512d86194b361d745eeaa836
prithveeraaja <133484943+prithveeraaja@users.noreply.github.com>
2025-02-13 19:20:23 +0000

    array

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,2 +1,40 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
-const dc_heros = ["superman "]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+
+// console.log(marvel_heros); //[ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+
+// console.log(marvel_heros[3][1]); // flash
+
+const allHeros = marvel_heros.concat(dc_heros)
+console.log(allHeros);  // [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+
+
+const allNewHeros =  [...marvel_heros, ...dc_heros]
+console.log(allNewHeros);//[ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+const anotherArray = [1, 2, 3, [4, 5, 6], 7,[6, 7,[4, 5]]]
+
+const realArray = anotherArray.flat(Infinity)
+console.log(realArray); // [1......5 ]
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name:"hitesh"})); // [] interesting case give empty 
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3)) // [100, 200, 300]
+
+
+
+
+
+
+
+
+
